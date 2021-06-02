021430fe65e5e368c2673b157f41a3da2ca59974
Pétur Karl Ingólfsson <peturkarlingolfsson@gmail.com>
2021-06-02 23:14:23 +0000

    Inital version strapi and nuxt communicating

---
+++ frontend/pages/login.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <v-col cols="8">
+        <v-toolbar color="pink">
+          <v-toolbar-title class="white--text">
+            UEFA Euro 2021 - National Teams
+          </v-toolbar-title>
+        </v-toolbar>
+        <v-card>
+          <v-form ref="form" lazy-validation>
+            <v-container>
+              <v-layout column>
+                <v-text-field label="Email" required></v-text-field>
+
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-layout>
+            </v-container>
+          </v-form>
+          <v-card-actions>
+            <v-flex>
+              <v-btn href="/vuetify/register">Register</v-btn>
+              <v-btn color="primary" @click="login">Sign In</v-btn>
+            </v-flex>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'auth',
+  data() {
+    return {
+      user: '',
+      pass: '',
+    }
+  },
+  methods: {
+    login() {
+      this.$strapi
+        .login({ identifier: this.user, password: this.pass })
+        .then((res) => {
+          if (res.user.id) {
+            this.$router.push('/')
+          }
+        })
+    },
+  },
+}
+</script>
